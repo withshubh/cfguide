2246dc73a35333bdbb9296c44472cc0d5be1b190
Raj Nandan Sharma <raj@CFs-MacBook-Pro-3.local>
2024-06-25 16:26:43 +0530

    feat(url): added guides in sub path

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,33 +7,33 @@ import { ColorBox } from "./components/colorbox"; // --> import the component
 import { theme } from "./theme";
 
 export const config = /*#__PURE__*/ configuration({
-  theme, // --> add the theme. modify `./theme.ts` for changing the theme.
-  dest: {
-    namespace: "", // --> your github pages namespace. remove if you are using a custom domain.
-  },
-  page: {
-    favicon: "/assets/images/favicon.ico",
-    title: {
-      base: "Cashfree Payments Developer Guides", // --> the base title of your doc pages
+    theme, // --> add the theme. modify `./theme.ts` for changing the theme.
+    dest: {
+        namespace: "/guides", // --> your github pages namespace. remove if you are using a custom domain.
     },
-    fonts: {
-      text: {
-        url: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
-        name: "Montserrat",
-      },
+    page: {
+        favicon: "/assets/images/favicon.ico",
+        title: {
+            base: "Cashfree Payments Developer Guides", // --> the base title of your doc pages
+        },
+        fonts: {
+            text: {
+                url: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+                name: "Montserrat",
+            },
+        },
     },
-  },
-  misc: {
-    github: {
-      user: "cashfree", // --> your github username (where your repo is hosted)
-      repo: "guides", // --> your github repo name
+    misc: {
+        github: {
+            user: "cashfree", // --> your github username (where your repo is hosted)
+            repo: "guides", // --> your github repo name
+        },
     },
-  },
-  plugins: [msClarity(), codingBlog(), customCSS(), customJS()],
-  markdown: {
-    customComponents: {
-      ...DefaultMarkdownCustomComponents,
-      ColorBox, // --> add the component to the markdown custom components
+    plugins: [msClarity(), codingBlog(), customCSS(), customJS()],
+    markdown: {
+        customComponents: {
+            ...DefaultMarkdownCustomComponents,
+            ColorBox, // --> add the component to the markdown custom components
+        },
     },
-  },
 });
