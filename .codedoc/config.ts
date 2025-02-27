9f424244f4cbcaffc673f6943233b22bdda18cf4
withshubh <withshubh@gmail.com>
2025-02-27 17:29:18 +0530

    updated config.ts

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,33 +7,33 @@ import { ColorBox } from "./components/colorbox"; // --> import the component
 import { theme } from "./theme";
 
 export const config = /*#__PURE__*/ configuration({
-    theme, // --> add the theme. modify `./theme.ts` for changing the theme.
-    dest: {
-        namespace: "/guides", // --> your github pages namespace. remove if you are using a custom domain.
+  theme, // --> add the theme. modify `./theme.ts` for changing the theme.
+  dest: {
+    namespace: "", // --> your github pages namespace. remove if you are using a custom domain.
+  },
+  page: {
+    favicon: "/assets/images/favicon.ico",
+    title: {
+      base: "Cashfree Payments Developer Guides", // --> the base title of your doc pages
     },
-    page: {
-        favicon: "/assets/images/favicon.ico",
-        title: {
-            base: "Cashfree Payments Developer Guides", // --> the base title of your doc pages
-        },
-        fonts: {
-            text: {
-                url: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
-                name: "Montserrat",
-            },
-        },
+    fonts: {
+      text: {
+        url: "https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap",
+        name: "Montserrat",
+      },
     },
-    misc: {
-        github: {
-            user: "cashfree", // --> your github username (where your repo is hosted)
-            repo: "guides", // --> your github repo name
-        },
+  },
+  misc: {
+    github: {
+      user: "withshubh", // --> your github username (where your repo is hosted)
+      repo: "cfguide", // --> your github repo name
     },
-    plugins: [msClarity(), codingBlog(), customCSS(), customJS()],
-    markdown: {
-        customComponents: {
-            ...DefaultMarkdownCustomComponents,
-            ColorBox, // --> add the component to the markdown custom components
-        },
+  },
+  plugins: [msClarity(), codingBlog(), customCSS(), customJS()],
+  markdown: {
+    customComponents: {
+      ...DefaultMarkdownCustomComponents,
+      ColorBox, // --> add the component to the markdown custom components
     },
+  },
 });
--- docs/assets/codedoc-bundle.entry.js
+++ docs/assets/codedoc-bundle.entry.js
@@ -1,31 +1,31 @@
-import { getRenderer } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
-import { initJssCs } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
-import { installTheme } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/content/theme.ts';installTheme();
-import { countCards } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
-import { codeSelection } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
-import { sameLineLengthInCodes } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
-import { initHintBox } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
-import { initCodeLineRef } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
-import { initSmartCopy } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
-import { copyHeadings } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
-import { contentNavHighlight } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
-import { loadDeferredIFrames } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
-import { smoothLoading } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
-import { tocHighlight } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
-import { postNavSearch } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
-import { copyLineLinks } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
-import { gatherFootnotes } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
-import { reloadOnChange } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/serve/reload.js';reloadOnChange();
-import { ArticleCard } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
-import { DarkModeSwitch } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
-import { ConfigTransport } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
-import { Author } from '/Users/shubhendra.chauhan/Desktop/repo/guides/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
+import { getRenderer } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/content/theme.ts';installTheme();
+import { countCards } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/count-cards.js';countCards();
+import { codeSelection } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { reloadOnChange } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/serve/reload.js';reloadOnChange();
+import { ArticleCard } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/article-card/index.js';
+import { DarkModeSwitch } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+import { Author } from '/Users/shubhendra.chauhan/Desktop/repo/cfguide/.codedoc/node_modules/@codedoc/coding-blog-plugin/dist/es5/components/author/index.js';
 
 const components = {
-  'xTzkv0RjpNF3MIAKJB60zA==': ArticleCard,
-  'TjCuw4ZU0vcdq+3DoVa9zA==': DarkModeSwitch,
-  'aCiYXsShHk4ZBdMxFKYPpA==': ConfigTransport,
-  'FAYsDhgNKxSHiWNRwsgaVg==': Author
+  'Zy0GrUaGlM5M5WQCdr+GRA==': ArticleCard,
+  'b6giU+MwYGYR12hbC9rYoQ==': DarkModeSwitch,
+  'aJJBRfS2IsSnxjtQ2f7lXg==': ConfigTransport,
+  'izf0HcCcxKSwZDNXrmTFCQ==': Author
 };
 
 const renderer = getRenderer();
